040dfecc05a931056f16ff2724ef3ad17e1940f7
TableStar <afraroad@gmail.com>
2025-11-07 13:53:42 +0700

    fix: changed to static

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,17 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import icon from 'astro-icon';
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), icon()],
-
+  output: "static",
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
